6abd29fdcd1931456ba0abd49f89bc6d7d15c9a9
Nicolas Dietermann <nicolas.dietermann@gmx.de>
2022-05-26 21:36:25 +0200

    Button gelöscht

--- app/src/main/java/com/appsbynico/fitnessplanner/MainActivity.java
+++ app/src/main/java/com/appsbynico/fitnessplanner/MainActivity.java
@@ -3,11 +3,9 @@ package com.appsbynico.fitnessplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
